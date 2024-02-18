30da44ab4d355ce82750743ef3cd89bed7912f21
Jakub Dranczewski <domdlapsa@gmail.com>
2024-02-18 00:01:34 +0000

    Change eventloop iteration scheduling
    Testing this as a possible fix for https://github.com/spyder-ide/spyder/issues/21299

--- ipykernel/kernelbase.py
+++ ipykernel/kernelbase.py
@@ -472,7 +472,7 @@ class Kernel(SingletonConfigurable):
             self.log.info("Exiting as there is no eventloop")
             return
 
-        def advance_eventloop():
+        async def advance_eventloop():
             # check if eventloop changed:
             if self.eventloop is not eventloop:
                 self.log.info("exiting eventloop %s", eventloop)
@@ -497,7 +497,7 @@ class Kernel(SingletonConfigurable):
             # flush the eventloop every so often,
             # giving us a chance to handle messages in the meantime
             self.log.debug("Scheduling eventloop advance")
-            self.io_loop.call_later(0.001, advance_eventloop)
+            self.io_loop.call_later(0.001, partial(self.schedule_dispatch, advance_eventloop))
 
         # begin polling the eventloop
         schedule_next()
